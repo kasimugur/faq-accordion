05ff3b43afc51f6c4940a4e4e56b39448e2ade4e
kasimugur <kasiugur7@gmail.com>
2024-07-11 11:58:55 +0300

    html components add and updated

---
+++ src/components/Card.jsx
@@ -0,0 +1,51 @@
+export default function Card() {
+  return (
+    <>
+      <div className="card">
+        <div className="acordion-item">
+
+          <div className="acordion-header">
+            <h2>What is Frontend Mentor, and how will it help me?
+              <button></button>
+            </h2>
+          </div>
+          <div className="acordion-body">
+            Frontend Mentor offers realistic coding challenges to help developers improve their
+            frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for
+            all levels and ideal for portfolio building.
+          </div>
+          <div className="acordion-header">
+            <h2>
+              Is Frontend Mentor free?
+
+              <button></button>
+            </h2>
+          </div>
+          <div className="acordion-body">
+            Yes, Frontend Mentor offers both free and premium coding challenges, with the free
+            option providing access to a range of projects suitable for all skill levels.
+          </div>
+          <div className="acordion-header">
+            <h2>
+              Can I use Frontend Mentor projects in my portfolio?
+              <button></button>
+            </h2>
+          </div>
+          <div className="acordion-body">
+            Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
+            way to showcase your skills to potential employers!
+          </div>
+          <div className="acordion-header">
+            <h2>
+              How can I get help if I'm stuck on a Frontend Mentor challenge?
+              <button></button>
+            </h2>
+          </div>
+          <div className="acordion-body">
+            How can I get help if I'm stuck on a Frontend Mentor challenge?
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
